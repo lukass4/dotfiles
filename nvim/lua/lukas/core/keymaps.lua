bfa7bbb9123c4638827d828506111e9469af12e1
Lukas <lukasjamesr4@gmail.com>
2025-08-01 21:39:46 +0100

    Neovim update

--- nvim/lua/lukas/core/keymaps.lua
+++ nvim/lua/lukas/core/keymaps.lua
@@ -17,3 +17,5 @@ keymap.set("n", "<leader>tx", "<cmd>tabclose<CR>", { desc = "Close current tab" 
 keymap.set("n", "<leader>tn", "<cmd>tabn<CR>", { desc = "Go to next tab" })
 keymap.set("n", "<leader>tp", "<cmd>tabp<CR>", { desc = "Go to previous tab" })
 keymap.set("n", "<leader>tf", "<cmd>tabnew %<CR>", { desc = "Open current buffer in new tab" })
+
+keymap.set("n", "<leader>q", ":wqa<CR>", { desc = "Writes and quits all open buffers" })
